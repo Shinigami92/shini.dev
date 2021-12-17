11ec0a43045af42cd692cdd45c6c205a4759462d
Shinigami92 <chrissi92@hotmail.de>
2021-12-17 23:00:20 +0100

    Add some basic content

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template lang="pug">
-h1 Shini's website soon™
+main
+  section
+    h1 Hi there 👋, I'm Shinigami aka Shini
+
+    h2 I'm a Frontend Developer working at #[a(href="https://www.adsoul.com/", target="_blank") adSoul GmbH] since May 2019
+
+    ul
+      li 🔭 I'm mainly working on #[a(href="https://github.com/prettier/plugin-pug", target="_blank") @prettier/plugin-pug], #[a(href="https://github.com/Shinigami92/eslint-plugin-vue-pug-sfc", target="_blank") eslint-plugin-vue-pug-sfc] and #[a(href="https://github.com/Shinigami92/eslint-define-config", target="_blank") eslint-define-config]
+      li ⚡ I'm team member of #[a(href="https://github.com/vitejs/vite", target="_blank") Vite]
+      li 🔍 I'm currently helping the Vite, Vue and Pug ecosystems to grow
+      li 📬 How to reach me: #[a(href="https://twitter.com/Shini_92", target="_blank") @Shini_92]
+      li ☕ If you like my work, you can #[a(href="https://www.paypal.com/donate?hosted_button_id=L7GY729FBKTZY", target="_blank") support me] with a coffee or beer 😀
 </template>
 
 <script lang="ts">
@@ -19,5 +30,9 @@ export default defineComponent({
   color: grey;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans,
     sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+}
+
+a {
+  color: lightskyblue;
 }
 </style>
